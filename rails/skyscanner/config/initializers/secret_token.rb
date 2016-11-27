7aa099b6d07bb556650e2924a38d4befac1763ad
J Bhaskar <jbhaskar1993@gmail.com>
2016-11-27 18:09:40 +0530

    regenerated application

--- rails/skyscanner/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Skyscanner::Application.config.secret_token = '8426e4a2087fb0735163c4d1bff0f10057209a10d72d644abb1193dfdb7913f2d662c2a9395d11216fda5ba71a6c32cb00acff5afbfe9fee69999d6caec79abf'
